8d2cb018924e3d19ad7369b6f796d5bca3f66b1a
Renato Filho <renatosousafilho@gmail.com>
2020-09-18 15:39:39 -0300

    substituindo aspas

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -7,5 +7,5 @@ db.movies.aggregate([
     }
   },
   { $project: { _id: false, diretor: '$_id', filmes: '$filmes' } },
-  { $sort: { filmes: 1, diretor: 1 } }
+  { $sort: { filmes: -1, diretor: 1 } }
 ]);
--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -2,22 +2,22 @@ db.movies.aggregate([
   {
     $match: {
       released: {
-        $gte: ISODate('1984-01-01'),
-        $lte: ISODate('1984-12-31')
+        $gte: ISODate("1984-01-01"),
+        $lte: ISODate("1984-12-31")
       },
-      genres: 'Drama'
+      genres: "Drama"
     }
   },
-  { $unwind: '$genres' },
+  { $unwind: "$genres" },
   {
     $group: {
-      _id: '$genres',
+      _id: "$genres",
       total: { $sum: 1 },
-      mediaIMDB: { $avg: '$imdb.rating' },
-      votosIMDB: { $sum: '$imdb.votes' }
+      mediaIMDB: { $avg: "$imdb.rating" },
+      votosIMDB: { $sum: "$imdb.votes" }
     }
   },
-  { $project: { _id: false, total: '$total', genres: true, mediaIMDB: { $round: ['$mediaIMDB', 1] }, votosIMDB: '$votosIMDB' } },
+  { $project: { _id: false, total: "$total", genres: true, mediaIMDB: { $round: ["$mediaIMDB", 1] }, votosIMDB: "$votosIMDB" } },
   { $sort: { total: -1 } },
   { $limit: 1 }
 ]);
